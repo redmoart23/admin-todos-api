e1c39b26bf31e9b60aacfac36f71125d3678db97
red <redmoart@gmail.com>
2025-03-17 16:02:43 -0500

    ♻️Refactor: Move toggleTodo function to actions and remove local implementation from TodosGrid

---
+++ src/todos/actions/todo-actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { Todo } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const toggleTodo = async (
+  id: string,
+  completed: boolean
+): Promise<Todo> => {
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  if (!todo) {
+    throw new Error(`Todo con id ${id} no existe`);
+  }
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: { completed },
+  });
+
+  revalidatePath("/dashboard/server-todos");
+
+  return updatedTodo;
+};
