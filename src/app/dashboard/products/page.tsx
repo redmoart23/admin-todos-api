7fe542fe969f7e953be1777b21e7aec16c6ff7ee
red <redmoart@gmail.com>
2025-03-21 16:55:23 -0500

    ✨Feature: Enhance Products page to dynamically render ProductCard components with product data

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,12 @@
 import { ProductCard } from "@/products/components/ProductCard";
+import { products } from "@/products/data/products";
 
 export default function ProductsPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-      <ProductCard />
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
     </div>
   );
 }
--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
+import { Product } from "../data/products";
 
-export const ProductCard = () => {
+export const ProductCard = ({ name, price, rating, image }: Product) => {
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       {/* Product Image */}
@@ -10,7 +11,7 @@ export const ProductCard = () => {
           width={500}
           height={500}
           className="rounded"
-          src="/images/products/1623735-00-A_0_2000.jpg"
+          src={image}
           alt="product image"
         />
       </div>
@@ -19,66 +20,38 @@ export const ProductCard = () => {
       <div className="px-5 pb-5">
         <a href="#">
           <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+            {name}
           </h3>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
           {/* Stars */}
-          <svg
-            className="w-5 h-5 text-yellow-300"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
 
-          <svg
-            className="w-5 h-5 text-yellow-300"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-
-          <svg
-            className="w-5 h-5 text-yellow-300"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-
-          <svg
-            className="w-5 h-5 text-yellow-300"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-
-          <svg
-            className="w-5 h-5 text-yellow-300"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
+          {[...Array(rating)].map((_, i) => (
+            <svg
+              key={i}
+              className="w-5 h-5 text-yellow-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M10 1l2.6 6.8H19l-5 4.2 1.8 7L10 14.3 4.6 18l1.8-7-5-4.2h6.4L10 1z"
+              ></path>
+            </svg>
+          ))}
 
           {/* Rating Number */}
           <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-            5.0
+            {rating}
           </span>
         </div>
 
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            $599
+            ${price}
           </span>
 
           <div className="flex">
